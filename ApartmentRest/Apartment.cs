bd9892313a6a3ccc1a257709e68795ac2bd971ec
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-10-03 13:15:31 +0200

    - Omdøbt IService1 og Service1 til "IApartmentService" og "ApartmentService". - Ændret i Web.config - Oprettet Apartment klassen. - Lavet prorperties og construktor i Apartment klassen.

---
+++ ApartmentRest/Apartment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApartmentRest
+{
+    public class Apartment
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Location { get; set; }
+        public int PostalCode { get; set; }
+        public int Size { get; set; }
+        public int NoRoom { get; set; }
+        public bool WashingMachine { get; set; }
+        public bool Dishwasher { get; set; }
+
+        public Apartment()
+        {
+            
+        }
+
+        public Apartment(int id, int price, string location, int postalCode, int size, int noRoom, bool washingMachine, bool dishwasher)
+        {
+            Id = id;
+            Price = price;
+            Location = location;
+            PostalCode = postalCode;
+            Size = size;
+            NoRoom = noRoom;
+            WashingMachine = washingMachine;
+            Dishwasher = dishwasher;
+        }
+    }
+}
